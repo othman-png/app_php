9f8b8a0bb89c581859c043af11b72f32df52e94f
othman <othman;ellkhayiwi@gmail.com>
2021-02-12 03:05:32 +0100

    projet

---
+++ error.php
@@ -0,0 +1,12 @@
+<style type="">
+	p {
+		font-size: 44;
+	}
+
+	body {
+		background-image: url(https://cdn.windowsreport.com/wp-content/uploads/2018/07/Error-message-1-886x590.jpg);
+	}
+
+</style>
+
+<p> BAD PARAMETER /TRY TO CREATE ACCOUNT IF DONT HAVE  IT </p>
--- menu.php
+++ menu.php
@@ -1,12 +1,29 @@
+<style >
+	ul {
+		font-size: 44;
+
+	}
+	
+	body {
+	background-image: url("https://img-0.journaldunet.com/DPAQb5w38KoXNDq63yCT562GwE4=/1280x/smart/18fef1b31ea9424a83d70ed7f7f02fac/ccmcms-jdn/11048405.jpg");
+	opacity: 10;
+	color: green;
+}
+</style>
  <nav id="menu">        
         <div class="element_menu">
-            <h3>navigur ver les otre pages </h3>
+
+            <h3>choise aa path </h3>
 
             <ul>
-                <li><a href="page1.html">Lien</a></li>
-                <li><a href="page2.html">Lien</a></li>
-                <li><a href="page3.html">Lien</a></li>
-                <li><a href="index.php">link</a> </li>
+            	
+                <li><a href="page1.html"> Networking </a></li>
+
+                <li><a href="page3.html"></a> web security</li>
+
+                <li><a href="index.php"></a>pentesting </li>
+
             </ul>
         </div>    
-  </nav>
+</nav>
+
